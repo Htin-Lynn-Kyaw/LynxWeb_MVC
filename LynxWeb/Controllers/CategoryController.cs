72aff3591312710b4f7e769468f52f3c97697bcb
William lynn <13thwerewolf@gmail.com>
2024-03-26 16:59:10 +0630

    26/03/2024

---
+++ LynxWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using LynxWeb.Data;
+using LynxWeb.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LynxWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategory = _db.Category.ToList();
+            return View(objCategory);
+        }
+    }
+}
---
+++ LynxWeb/Migrations/20240326091847_AddCategoryDatas.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LynxWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryDatas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DisplayOrder",
+                table: "Category",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "ID", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Action" },
+                    { 2, 2, "SciFi" },
+                    { 3, 3, "Horror" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DisplayOrder",
+                table: "Category",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
