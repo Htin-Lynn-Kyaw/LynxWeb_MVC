93f2b2141f0f8e5483e98ed593b85e5ad53e61e4
William lynn <13thwerewolf@gmail.com>
2024-04-04 11:02:36 +0630

    CrudRazor

--- LynxWebRazor/Pages/Categories/Edit.cshtml.cs
+++ LynxWebRazor/Pages/Categories/Edit.cshtml.cs
@@ -2,23 +2,21 @@ using LynxWebRazor.Data;
 using LynxWebRazor.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace LynxWebRazor.Pages.Categories
 {
     [BindProperties]
-    public class CreateModel : PageModel
+    public class EditModel : PageModel
     {
         private readonly ApplicationDbContext _db;
-        
         public Category Category { get; set; }
-        public CreateModel(ApplicationDbContext db)
+        public EditModel(ApplicationDbContext db)
         {
             this._db = db;
         }
         public void OnGet(int? id)
         {
-            if(id != null)
+            if (id != null)
             {
                 Category? obj = _db.Category.Find(id);
                 Category = obj;
@@ -26,15 +24,14 @@ namespace LynxWebRazor.Pages.Categories
         }
         public IActionResult OnPost()
         {
-            _db.Category.Add(Category);
-            _db.SaveChanges();
-            return RedirectToPage("Index");
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(Category);
+                _db.SaveChanges();
+                //TempData["success"] = "Successfully Edited.";
+                return RedirectToPage("Index");
+            }
+            return Page();
         }
-        //public IActionResult OnPost()
-        //{
-        //    _db.Category.Add(Category);
-        //    _db.SaveChanges();
-        //    return RedirectToPage("Index");
-        //}
     }
 }
